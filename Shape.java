461990291eedcb7198b05169e6495911ac9692d9
busenurhasdemir <busenurhasdemir@posta.mu.edu.tr>
2025-04-17 13:20:51 +0300

    week 8

---
+++ Shape.java
@@ -0,0 +1,38 @@
+public class Shape {
+    private String color = "white";
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    private double area() {
+        return 0.0;
+    }
+
+    public double primeter() {
+        return 0.0;
+    }
+
+    public void describe(int sideA, int sideB) {
+        System.out.println("Side a is" + sideA + "Side b is" + sideB);
+    }
+
+    public void describe(int sideA) {
+        System.out.println("Side a is" + sideA);
+    }
+
+    public void describe(double sideA) {
+        System.out.println("Side a is" + sideA);
+    }
+    public void describe(){
+        System.out.println("This is a shape");
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
